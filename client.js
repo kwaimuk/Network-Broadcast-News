7961cddeda9dc60c5c96d56e25d82015e0f8ce93
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-04-13 13:47:22 -1000

    users now can change the user name

--- client.js
+++ client.js
@@ -3,15 +3,12 @@
 const net = require('net');
 
 const client = net.connect ({port: 6969}, () => {
-
   client.on('data',(data) => {
-
     console.log(data.toString());
   });
 
 process.stdin.on('data',(data) => {
-
-    client.write(data.toString());
+ client.write(data.toString());
   });
 
     client.on ('end', () => {
--- server.js
+++ server.js
@@ -6,17 +6,42 @@ const net = require('net');
 
 const server = net.createServer((connection) => {
 
-console.log(`CONNECTED: ${connection.localAddress}:${connection.remotePort}`);
-users.push(connection);
-connection.write(`Hello there are ${users.length} of Users!`);
+  console.log(`CONNECTED: ${connection.localAddress}:${connection.remotePort}`);
+  users.push(connection);
+  connection.write(`Hello there are ${users.length} of Users!`);
+  connection.write(" Type /username to make a username");
+
+  // connection.once('data', (data)=> {
+  //   connection.name = data.toString().trim();
+  // });
 
   connection.on('data', (data)=> {
 
     console.log(`SERVER BCAST FROM${connection.localAddress}:${connection.remotePort}: ${data}`);
+    const defaultName = `${connection.localAddress}:${connection.remotePort}`;
+
+    let displayName =connection.name;
+
+    if(connection.name===undefined){
+       displayName = defaultName;
+    }
+      const words=data.toString();
+      const userNameTrimmed = words.slice(0,9);
+
+    if(userNameTrimmed === '/username'){
+      console.log("watup");
+      const userNameTrims = words.slice(9);
+      console.log(userNameTrims);
+      connection.name= userNameTrims;
+    }
+
     for (var i = 0; i < users.length; i++) {
-        users[i].write(`${connection.localAddress}:${connection.remotePort}: ${data}`);
-        }
-      });
+      if(users[i].remotePort !== connection.remotePort){
+                users[i].write(`${displayName}: ${data}`);
+              }
+            }
+          });
+
 
   connection.on ('end', () => {
     console.log('connection from client is closed');
